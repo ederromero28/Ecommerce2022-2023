c3a90bc8fed5c62780cb79e5cb4801bd1877b524
Eder Romero <ederromero32@gmail.com>
2022-11-11 16:40:40 -0500

    primer inicio del ecommerce

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const bar = document.getElementById('bar');
+const cerrar = document.getElementById('cerrar');
+const nav = document.getElementById('navbar');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+if (cerrar) {
+    cerrar.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
